e840fd4c0936f0ea9a58456740001f826d4c2690
jacquikeating <j.keating@live.ca>
2025-07-18 15:45:20 -0400

    Added conditional .held-die style

--- Die.jsx
+++ Die.jsx
@@ -1,6 +1,6 @@
-export default function Die({value}) {
+export default function Die({value, isHeld, id}) {
     return (
-        <button className="die">
+        <button className={isHeld ? ("die held-die") : ("die")}>
             <p className="die-value">{value}</p>
         </button>
     )
