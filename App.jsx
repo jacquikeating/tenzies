b7a2bf8332a16691d858ffcc9b90756bef7741e3
jacquikeating <j.keating@live.ca>
2025-07-18 13:03:03 -0400

    Added .map() function to App to render 10 Die components

--- App.jsx
+++ App.jsx
@@ -1,5 +1,13 @@
 import Die from "./Die.jsx"
 
 export default function App() {
-    return <main></main>
+    const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    return (
+        <section id="dice-container">
+            {nums.map((num) => {
+                return <Die key={num} value={(Math.floor(Math.random() * 6) + 1)} />
+            })}
+        </section>
+    )
 }
