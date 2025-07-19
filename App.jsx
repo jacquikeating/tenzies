9c668f854748332ef8c37dd7e8737415b0b5ce0c
jacquikeating <j.keating@live.ca>
2025-07-19 00:16:18 -0400

    Added confetti when game is won
    - Installed react-confetti package.
    - Conditionally rendered <Confetti> component when gameWon is true.

--- App.jsx
+++ App.jsx
@@ -1,67 +1,79 @@
 import { useState, useRef, useEffect } from "react";
+import Confetti from "react-confetti";
 import Die from "./Die.jsx";
 
 export default function App() {
-    const [diceArr, setDiceArr] = useState(generateDice());
-    const rollBtn = useRef(null)
+  const [diceArr, setDiceArr] = useState(generateDice());
+  const rollBtn = useRef(null);
 
-    let gameWon =
-        diceArr.every((die) => die.isHeld) &&
-        diceArr.every((die) => die.value === diceArr[0].value);
+  let gameWon =
+    diceArr.every((die) => die.isHeld) &&
+    diceArr.every((die) => die.value === diceArr[0].value);
 
-    function generateDice() {
-        return new Array(10).fill(0).map((dieObject, index) => ({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: index,
-        }));
-    }
+  function generateDice() {
+    return new Array(10).fill(0).map((dieObject, index) => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: index,
+    }));
+  }
 
-    function holdDie(dieID) {
-        setDiceArr((prevDiceArr) =>
+  function holdDie(dieID) {
+    setDiceArr((prevDiceArr) =>
+      prevDiceArr.map((die) =>
+        die.id === dieID ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
+
+  function rollDice() {
+    if (gameWon) {
+      setDiceArr(generateDice());
+    } else {
+      setDiceArr((prevDiceArr) =>
         prevDiceArr.map((die) =>
-            die.id === dieID ? { ...die, isHeld: !die.isHeld } : die
+          die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
         )
-        );
+      );
     }
+  }
 
-    function rollDice() {
-        if (gameWon) {
-            setDiceArr(generateDice())
-        } else {
-            setDiceArr(prevDiceArr => prevDiceArr.map(die =>
-                die.isHeld ? die : {...die, value: Math.ceil(Math.random() * 6)}
-        ))}  
-    }
+  useEffect(() => {
+    rollBtn.current.focus();
+  }, [gameWon]);
 
-    useEffect(() => {
-        rollBtn.current.focus()
-    }, [gameWon])
+  return (
+    <main>
+      {gameWon && <Confetti />}
+      <div aria-live="polite" className="sr-only">
+        {gameWon && (
+          <p>Congratulations! You won! Press "New Game" to start again.</p>
+        )}
+      </div>
 
-    return (
-        <main>
-            <div aria-live="polite" className="sr-only">
-                {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
-            </div>
-            
-            <h1 className="title">Tenzies</h1>
-            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-            
-            <section id="dice-container">
-                {diceArr.map((die) => {
-                    return <Die 
-                        key={die.id} 
-                        value={die.value} 
-                        isHeld={die.isHeld}
-                        id={die.id}
-                        holdDie={holdDie}
-                        />
-                })}
-            </section>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
 
-            <button className="roll-btn" onClick={rollDice} ref={rollBtn}>
-                {gameWon ? "New Game" : "Roll the dice!"}
-            </button>
+      <section id="dice-container">
+        {diceArr.map((die) => {
+          return (
+            <Die
+              key={die.id}
+              value={die.value}
+              isHeld={die.isHeld}
+              id={die.id}
+              holdDie={holdDie}
+            />
+          );
+        })}
+      </section>
+
+      <button className="roll-btn" onClick={rollDice} ref={rollBtn}>
+        {gameWon ? "New Game" : "Roll the dice!"}
+      </button>
     </main>
   );
 }
