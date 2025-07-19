c9d3688c72bc3cc7f3f6337737569452225c5db9
jacquikeating <j.keating@live.ca>
2025-07-18 22:16:40 -0400

    Style updates
    - Fixed a few stray CSS styles that did not match Figma design.
    - Installed vite package.
    - Prettier automatically threw some semicolons into App.jsx.

--- App.jsx
+++ App.jsx
@@ -1,55 +1,62 @@
-import { useState } from "react"
-import Die from "./Die.jsx"
+import { useState } from "react";
+import Die from "./Die.jsx";
 
 export default function App() {
-    const [diceArr, setDiceArr] = useState(generateDice())
+  const [diceArr, setDiceArr] = useState(generateDice());
 
-    let gameWon = diceArr.every(die => die.isHeld) && diceArr.every(die => die.value === diceArr[0].value)
+  let gameWon =
+    diceArr.every((die) => die.isHeld) &&
+    diceArr.every((die) => die.value === diceArr[0].value);
 
-    function generateDice() {
-        return new Array(10)
-            .fill(0)
-            .map((dieObject, index) => ({
-                value: Math.ceil(Math.random() * 6),
-                isHeld: false,
-                id: index
-            }))
-    }
+  function generateDice() {
+    return new Array(10).fill(0).map((dieObject, index) => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: index,
+    }));
+  }
 
-    function holdDie(dieID) {
-        setDiceArr(prevDiceArr => prevDiceArr.map(die => 
-            die.id === dieID ? {...die, isHeld: !die.isHeld} : die
-        ))
-    }
+  function holdDie(dieID) {
+    setDiceArr((prevDiceArr) =>
+      prevDiceArr.map((die) =>
+        die.id === dieID ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
 
-    function rollDice() {
-        setDiceArr(prevDiceArr => prevDiceArr.map(die =>
-            die.isHeld ? die : {...die, value: Math.ceil(Math.random() * 6)}
-        ))
-    }
+  function rollDice() {
+    setDiceArr((prevDiceArr) =>
+      prevDiceArr.map((die) =>
+        die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+      )
+    );
+  }
 
-    return (
-        <main>
-            <section id="intro">
-                <h1 className="title">Tenzies</h1>
-                <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-            </section>
-            
-            <section id="dice-container">
-                {diceArr.map((die) => {
-                    return <Die 
-                        key={die.id} 
-                        value={die.value} 
-                        isHeld={die.isHeld}
-                        id={die.id}
-                        holdDie={holdDie}
-                        />
-                })}
-            </section>
+  return (
+    <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
 
-            <button className="roll-btn" onClick={rollDice}>
-                {gameWon ? "New Game" : "Roll the dice!"}
-            </button>
-        </main> 
-    )
+      <section id="dice-container">
+        {diceArr.map((die) => {
+          return (
+            <Die
+              key={die.id}
+              value={die.value}
+              isHeld={die.isHeld}
+              id={die.id}
+              holdDie={holdDie}
+            />
+          );
+        })}
+      </section>
+
+      <button className="roll-btn" onClick={rollDice}>
+        {gameWon ? "New Game" : "Roll the dice!"}
+      </button>
+    </main>
+  );
 }
